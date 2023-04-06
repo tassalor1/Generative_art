d576257106fb09946dbfb7e50ce83483dc6aa8f2
tassalor1 <walkerconnor14@gmail.com>
2023-04-06 22:42:59 +0100

    Experimenting

--- pixel-gen.py
+++ pixel-gen.py
@@ -7,12 +7,12 @@ def colors():
     ]
     return blue_group[0]
 
-width, height = 800, 800
-fig = plt.figure(figsize=(width/80, height/80), dpi=90)
+width, height = 900, 900
+fig = plt.figure(figsize=(width/90, height/80), dpi=90)
 ax = plt.axes(xlim=(0, width), ylim=(0, height))
 
 # Create the pixelated background
-pixel_size = width // 10
+pixel_size = width // 20 # Amount of pixel squares
 for i in range(0, width, pixel_size):
     for j in range(0, height, pixel_size):
         rect = plt.Rectangle((i, j), pixel_size, pixel_size, linewidth=0, color=(252/255,218/255,139/255 ))
@@ -20,9 +20,9 @@ for i in range(0, width, pixel_size):
 
 # Draw grid lines
 for i in range(0, width, pixel_size):
-    ax.vlines(i, 0, height, colors='black', linewidth=3.5)
+    ax.vlines(i, 0, height, colors='black', linewidth=3.0)
 for j in range(0, height, pixel_size):
-    ax.hlines(j, 0, width, colors='black', linewidth=3.5)
+    ax.hlines(j, 0, width, colors='black', linewidth=3.0)
 
 ax.set_xticks([])
 ax.set_yticks([])
